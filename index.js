e7e2aa1897596cf3705bef8ab43738d11de97f8d
chpeet <bommbala8@gmail.com>
2024-09-21 03:18:18 +0100

    fixed some issues

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 mongoose
   .connect(
-   "mongodb+srv://bommbala8:test123@farm.tbayu.mongodb.net/?retryWrites=true&w=majority&appName=farm"
+   "mongodb+srv://bommbala8:test123@farm.tbayu.mongodb.net/farm?retryWrites=true&w=majority"
   );
 
 const staffs = [];
